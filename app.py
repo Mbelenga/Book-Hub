b06eacd4a2831a24546f3b22e8594ffa512145d7
Mbelenga <mbelengaterence@gmail.com>
2024-05-28 08:27:02 -0700

    Update all

--- app.py
+++ app.py
@@ -1,11 +1,62 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 from book_search import search_books, search_books_by_category
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 @app.route('/')
 def home():
-    return render_template('user.html')
+    return render_template('modern.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    hashed_password = generate_password_hash(password, method='sha256')
+    new_user = User(name=name, email=email, password=hashed_password)
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created successfully!', 'success')
+        return redirect(url_for('home'))
+    except:
+        flash('Email address already exists!', 'danger')
+        return redirect(url_for('home'))
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email).first()
+
+    if user and check_password_hash(user.password, password):
+        session['user_id'] = user.id
+        session['user_name'] = user.name
+        flash('Successfully logged in!', 'success')
+        return redirect(url_for('user_dashboard'))
+    else:
+        flash('Login failed. Check your email and password.', 'danger')
+        return redirect(url_for('home'))
+
+@app.route('/user_dashboard')
+def user_dashboard():
+    if 'user_id' in session:
+        return render_template('user.html', name=session['user_name'])
+    else:
+        return redirect(url_for('home'))
 
 @app.route('/categories')
 def categories():
@@ -16,10 +67,10 @@ def category(category_name):
     books = search_books_by_category(category_name)
     formatted_books = [
         {
-            "title": book['volumeInfo'].get('title', 'No title available'),
-            "authors": book['volumeInfo'].get('authors', ['No authors available']),
-            "thumbnail": book['volumeInfo'].get('imageLinks', {}).get('thumbnail', ''),
-            "description": book['volumeInfo'].get('description', 'No description available')
+            "title": book['title'],
+            "authors": book['authors'],
+            "thumbnail": book['cover']['medium'] if 'cover' in book else '',
+            "description": book.get('description', 'No description available')
         }
         for book in books
     ]
@@ -51,4 +102,5 @@ def search():
     return jsonify(books=formatted_books)
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
